431f702300881679c2dea1368038d3e017a7b6b5
akenaton9404 <akenatotartaglione@gmail.com>
2021-03-31 11:20:23 +0200

    Update calcolo_divisori.c

--- Esercizi/calcolo_divisori.c
+++ Esercizi/calcolo_divisori.c
@@ -1,20 +1,22 @@
 #include<stdio.h>
 
-int numero_pad(int a)       //pad = perfetto, abbondante, difettivo
+//funzione per trovare numeri: perfetti, abbondanti e difettivi
+int numero_pad(int a)
 {
     int somma = 0;
 
+    //ciclo per trovare i divisori
     for (int i=a-1; i>=1; i--)
        {
           if(a%i==0)
             somma += i;
        }
 
-    if(somma == a)
+    if(somma == a) //perfetto
       return 0;
-    if(somma > a)
+    if(somma > a) //abbondante
       return 1;
-    if(somma < a)
+    if(somma < a) //difettivo
       return -1;
 }
 
@@ -26,19 +28,19 @@ int main()
     printf("Inserire il numero di cui calcolare i divisori: ");
     scanf("%d", &n);
 
-    for(i = 0; i < 20; i++)
+    for(i = 0; i < 20; i++) //imposta tutto l'array a 0 per comodità
     {
       perfetti[i] = 0;
     }
 
-    for(i = 1; i <= n; i++)
+    for(i = 1; i <= n; i++) //controlla tutti i numeri inferiori ad n
     {
-        if(numero_pad(i) == 0)
+        if(numero_pad(i) == 0) //posiziona i numeri perfetti nell'array
         {
           perfetti[cont] = i;
           cont++;
         }
-        if(numero_pad(i) == 1)
+        if(numero_pad(i) == 1) //stampa dei numero abbondanti fino al primo dispari
         {
           if(i % 2 == 1)
           {
@@ -55,11 +57,11 @@ int main()
       printf("\nNon è stato trovato nessun numero perfetto\n");
     else
     {
-      for(i = 0; i < cont; i++)
+      for(i = 0; i < cont; i++) //somma tutti i numeri perfetti
       {
         somma += perfetti[i];
       }
-      for(i = 0; i < cont; i++)
+      for(i = 0; i < cont; i++) //controlla e stampa i numeri perfetti maggiori della media
       {
         if(perfetti[i] > somma/cont)
           printf("\nNumero perfetto maggiore della media: %d\n", perfetti[i]);
