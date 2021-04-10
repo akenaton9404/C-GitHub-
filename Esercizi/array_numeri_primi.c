ad2460e2ae7588874bc90b57d7e1871a92a47844
akenaton9404 <akenatotartaglione@gmail.com>
2021-04-10 11:01:53 +0200

    Update array_numeri_primi.c

--- Esercizi/array_numeri_primi.c
+++ Esercizi/array_numeri_primi.c
@@ -2,18 +2,15 @@
 #include <stdlib.h>
 #define lunghezza_vettore 1000
 
-void stampa(int a[])
+void stampa(int a[], int b)
 {
-    for(int i = 0; i<lunghezza_vettore; i++)
+    for(int i = 0; i < b; i++)
     {
         if(i%10==0)
             printf("\n\n");
-        
+
         if(a[i]!=0)
-            printf("%d\t", a[i]); 
-        else
-            printf(" \t");
-        
+            printf("%d\t", a[i]);
     }
 }
 
@@ -21,15 +18,21 @@ int n_primo(int a)
 {
     int somma=0;
 
-    for (int i=a; i>=1; i--)
+    for (int i = a/2; i >= 1; i--)  // a/2 perchè un numero non ha divisori maggiori della sua metà
     {
-      if(a%i==0)
+      if(a % i == 0)
         somma += i;
-    }  
-    
-    if(somma==a+1)
+    }
+
+    // 17; somma = 1
+    // 19; somma = 1
+    // 23; somma = 1
+
+    // a; somma = 1 (se il numero è primo)
+
+    if(somma == 1)
         return 1;
-    else 
+    else
         return 0;
 }
 
@@ -37,37 +40,31 @@ int n_primo(int a)
 int main()
 {
     int vet[lunghezza_vettore], vet2[lunghezza_vettore];
-    int i, out=0, puntatore=0;
+    int i, out = 0, puntatore = 0;
 
     for(i = 0; i < lunghezza_vettore; i++)
     {
-      vet2[i] = 0;
-    }
-
-    for (i = 0; i<lunghezza_vettore; i++)
-    {
-        vet[i] = (rand()%1000)+1;
-        
-    }
-    stampa(vet);
-    i=0;
-    
-    for(i=0; i<lunghezza_vettore; i++)
-    {
-        if(n_primo(vet[i])==1)
+        //vet[i] = (rand()%1000)+1; per accorciare il codice si può inserire direttamente nei parametri in input della funzione
+        if(n_primo(vet[i] = (rand()%1000)+1)==1)
         {
             vet2[puntatore]=vet[i];
             out=1;
             puntatore++;
         }
+        else
+        {
+          vet2[i] = 0;
+        }
     }
+
+    stampa(vet, lunghezza_vettore);
 
     if(out==0)
         printf("\n\nnon sono stati trovati numeri primi");
     else
     {
         printf("\n\nsono presenti numeri primi");
-        stampa(vet2);
+        stampa(vet2, puntatore);
     }
     return 0;
 }
