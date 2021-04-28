83d83b768e302f10701152898f9cc5e06c2ed951
akenaton9404 <akenatotartaglione@gmail.com>
2021-04-28 09:51:05 +0200

    Update array_numeri_primi.c

--- Esercizi/array_numeri_primi.c
+++ Esercizi/array_numeri_primi.c
@@ -6,10 +6,10 @@ void stampa(int a[], int b)
 {
     for(int i = 0; i < b; i++)
     {
-        if(!(i%10))
+        if(!(i%10)) 
             printf("\n\n");
 
-        if(a[i] != 0)
+        if(a[i]) // = if(a[i] != 0)
             printf("%d\t", a[i]);
     }
 }
@@ -33,7 +33,6 @@ int n_primo(int a)
         return 0;
 }
 
-
 int main()
 {
     int vet_rand[lunghezza_vettore], vet_primi[lunghezza_vettore];
@@ -43,27 +42,26 @@ int main()
     {
         vet_rand[i] = (rand()%1000)+1;
 
-        if(n_primo(vet_rand[i])) //in assembly gli if vengono eseguiti il la codizione è diversa da 0 
+        if(n_primo(vet_rand[i])) //in assembly gli if vengono eseguiti il la codizione è diversa da 0. Nel nostro caso deve eseguire se la funzione ritorna 1
         {
             vet_primi[puntatore] = vet_rand[i];
             out = 1;
             puntatore++;
         }
         else
-        {
           vet_primi[i] = 0;
-        }
     }
 
     stampa(vet_rand, lunghezza_vettore);
 
-    if(!out)
-        printf("\n\n===================================\nnon sono stati trovati numeri primi\n===================================");
-    else
+    if(out)
     {
         printf("\n\n==========================\nsono presenti numeri primi\n==========================");
         stampa(vet_primi, puntatore);
     }
+    else
+        printf("\n\n===================================\nnon sono stati trovati numeri primi\n===================================");
+
     
     return 0;
 }
